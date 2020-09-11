fb09e99cf2922d8a8a39b0f7150f2e450043fe35
feelren <54058978+feelren@users.noreply.github.com>
2020-09-11 23:28:30 +0300

    1

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ closeBtn.addEventListener('click', function (e) {
     closeBtn.style.display = 'none';
     openBtn.style.display = 'flex'
 })
+
+
+
+
+
+let media425 = window.matchMedia('(max-width: 425px)');
+let body = document.querySelector('body');
+let nav = document.querySelector('.nav');
+let navLogo = document.querySelector('.nav__logo');
+
+
+
+
+
+// setInterval(func, 1);
+
+// function func() {
+//     if (media425.matches) {
+//         // body.style.backgroundColor = 'white';
+
+//         if (nav.offsetTop > 646) {
+//             navLogo.style.display = 'none';
+//         } else navLogo.style.display = 'block';
+//     }
+// }
